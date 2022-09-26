6d405e51b87e147ee3b6138688d6cdd282f1f804
farah <farah.fournat@gmail.com>
2022-09-26 23:35:29 +0200

    added DateIcon

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,5 @@
-import Link from "next/link";
 import Button from "../ui/button";
-
+import DateIcon from "../icons/date-icon";
 import classes from "./event-item.module.css";
 
 function EventItem(props) {
@@ -22,6 +21,7 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
