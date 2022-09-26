a026b76bd8144b519f7f2961b50a98e56c650b84
farah <farah.fournat@gmail.com>
2022-09-26 23:43:02 +0200

    added ArrowIcon

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,7 @@
 import Button from "../ui/button";
 import DateIcon from "../icons/date-icon";
 import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 import classes from "./event-item.module.css";
 
 function EventItem(props) {
@@ -31,7 +32,12 @@ function EventItem(props) {
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
