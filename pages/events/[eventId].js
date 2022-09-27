84e50af9bc7f5d892b193425cf382d742730f88e
farah <farah.fournat@gmail.com>
2022-09-27 09:54:35 +0200

    added EventLogistics component

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,8 @@
 import { useRouter } from "next/router";
 import { Fragment } from "react";
 import { getEventById } from "../../dummy-data";
-import { EventSummary } from "../../components/event-detail/event-summary";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
 
 function EventDetailPage() {
   const router = useRouter();
